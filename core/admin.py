9e9f2770c91b2b267998697fa179a5d5c749bad4
anakindinger <anakindinger@gmail.com>
2022-08-04 16:27:21 -0300

    'Atualização models'

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,11 @@
 import email
 from django.contrib import admin
 
-from .models import Participante
+from .models import Formacao, Participante
 
 class ParticipanteAdmin(admin.ModelAdmin):
     list_display = ('nome_completo', 'email')
 
 
 admin.site.register(Participante, ParticipanteAdmin)
+admin.site.register(Formacao)
--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Participante(models.Model):
     nome_completo = models.CharField('Nome completo', max_length=100)
     email = models.EmailField('E-mail', max_length=70)
+    #formacoes = models.ManyToManyField(Formacao, through = 'FormacaoParticipantes')
 
     def __str__(self):
         return f'Nome : {self.nome_completo}, E-mail: {self.email}'
@@ -12,5 +13,10 @@ class Participante(models.Model):
 
 class Formacao(models.Model):
     titulo = models.CharField('Titulo', max_length = 300)
-    data = models.DateField('%d/%m/%Y')
-    duracao = models.DurationField('%H:%M')
+    data = models.DateField('Data')
+    duracao = models.DurationField('Duração')
+    participantes = models.ManyToManyField(Participante, through = 'FormacaoParticipantes')
+
+class FormacaoParticipantes(models.Model):
+    formacao = models.ForeignKey('Formacao', on_delete = models.CASCADE)
+    participantes = models.ForeignKey('Participante', on_delete = models.CASCADE)
